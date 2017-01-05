96326639c8f968f33f64dc4028c5209129d37415
Qian Chen <oda_san@hotmail.com>
2017-01-05 12:14:43 -0800

    stuff

---
+++ main.js
@@ -0,0 +1,3 @@
+var utils = require('./utils.js');
+
+console.log(utils.parse('name,age\nharry,12'));
---
+++ utils.js
@@ -0,0 +1,60 @@
+
+/***** EXAMPLE *****
+
+function parse(str) {
+  var keysAndValues = str.split('\n');
+  var keys = keysAndValues[0].split(',');
+  var values = keysAndValues[1].split(',');
+  var obj = {};
+
+  for (var i=0; i< keys.length; i++) {
+    var key = keys[i];
+    var value = values[i];
+    obj[key] = value;
+  }
+  return obj;
+}
+
+module.exports = {
+  parse: parse
+}
+
+***************/
+
+var str = '/pets?cute=true&big=false';
+/* {
+  path: '/board',
+  query: {
+    row: 'a',
+    col: 'b'
+  }
+} */
+
+function parseURL(str) {
+  var pathAndQuery = str.split('?');
+  var path = pathAndQuery[0];
+  var queryKeysValues = pathAndQuery[1].split('&');
+  var queryObj = {};
+  var obj = {};
+
+  queryKeysValues.forEach(function(keyValPair) {
+    var key = keyValPair.split('=')[0];
+    var value = keyValPair.split('=')[1];
+    queryObj[key] = value;
+  });
+  obj.path = path;
+  obj.query = queryObj;
+
+  console.log(obj);
+}
+
+function toURL() {
+
+}
+
+module.exports = {
+  parseURL: parseURL,
+  toURL: toURL
+};
+
+parseURL(str);
